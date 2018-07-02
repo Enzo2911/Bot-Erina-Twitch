2a318c63522129567c840a47ce576dfb0d147ba4
Enzo2911 <38391212+Enzo2911@users.noreply.github.com>
2018-07-02 19:49:12 +0200

    Add files via upload

---
+++ Erina-Twitch/index.js
@@ -0,0 +1,134 @@
+const tmi = require('tmi.js');
+const prefix = "!";
+const reactions = require('./reactions.js');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://flashzo:bbenzo2911@ds121861.mlab.com:21861/flashzo');
+var Quotes = ["\Abandonner est un signe de lacheté", "\" C est en encourageant l’individu qu’on révèle et développe ses meilleurs dons.", "\" Soyez celui qui voit la vie du bon côté et en parle avec crédibilité.",  "\" \Vers l'infini et au dela",  "\" I WILL BE BACK","\" Que la force soit avec toi","\" This is partaaaaaa!!!","\" NOM DE DIEU ", "\" \JE SUIS TON PERE", "\"JE SUIS GROUT ","\" ON DIT MERCI QUI, TON PERE ","\"Pikatchu attaque",];
+
+const tmiConfig = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect:  true
+    },
+    identity: {
+        username: "erinatwitchbot",
+        password: "oauth:frfk0bd216infrem20gnyp1l9d2z19"
+    },
+    channels: [
+        "flashzo12345"
+    ]
+};
+
+let client = new tmi.client(tmiConfig);
+
+client.connect();
+
+client.on('connected', (adress, port) => {
+    console.log(client.getUsername() + " s'est connecté sur : " + adress + ", port : " + port);
+    client.say("votre nom de chaine", "Bonjour Twitch Je suis en train de créé un bot");
+});
+
+
+
+function commandParser(message){
+    let prefixEscaped = prefix.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+    let regex = new RegExp("^" + prefixEscaped + "([a-zA-Z]+)\s?(.*)");
+    return regex.exec(message);
+}
+function isSubscriber(user){
+    return user.subscriber;
+}
+function isModerator(user){
+    return user.mod;
+}
+function isBroadcaster(user){
+    return user.badges.broadcaster == '1';
+}
+client.on("chat", function (channel, user, message, self) {
+	
+    if (self) return;
+
+    let sender = user['display-name'];
+
+    if(user['mod'] === false){
+        if(message.includes("http:") || message.includes(".com") || message.includes("https:") || message.includes("www.") || message.includes(".fr")){
+            client.timeout(channel, sender, 60, "LIEN INTERDIT !!");
+			client.say(channel, "LIEN INTERDIT : " + user['display-name'] + " / Banni pour 60 seconde")
+        }
+    }
+
+    if(user['mod'] === false){
+        if(message.includes('fdp') || message.includes('salope') || message.includes('ntm') || message.includes('Fdp') || message.includes('Salope') || message.includes('NTM')|| message.includes('Ntm')|| message.includes('FDP')|| message.includes('SALOPE')){
+            client.timeout(channel, sender, 180, "MOT INTERDIT !!");
+			client.say(channel, "MOT INTERDIT : " + user['display-name'] + " / Banni pour 180 seconde")
+     
+        }
+    }
+
+    if(message == "!youtube") {
+        client.say(channel, "Le lien youtube");
+    }
+  if (message == "!quote") {
+    	var rand = Quotes[Math.floor(Math.random() * Quotes.length)];
+		client.say(channel, rand);
+	}
+  
+ client.action()
+   
+});
+client.on('chat', (channel, user, message, isSelf) => {
+    if (isSelf) return;
+
+    let fullCommand = commandParser(message);
+    
+    if(fullCommand){
+        let command = fullCommand[1];
+        let param = fullCommand[2];
+        
+        switch(command){
+            case "bonjour":
+    if(isModerator(user)){
+        client.say(channel, "Bonjour " + user['display-name'] + ", mon rôle de Bot est de t'aider a géré le live !");
+    } else if(isBroadcaster(user)){
+        client.say(channel, "Bonjour " + user['display-name'] + ", je suis content que tu est  penser a moi !");
+    } else if(isSubscriber(user)){
+        client.say(channel, "Bonjour " + user['display-name'] + ", merci d'avoir souscris à cette chaine !");
+    } else {
+        client.say(channel, "Bonjour à toi " + user['display-name'] + ", sois le bienvenue sur ce live !");
+    }
+                break;
+		    case "help":
+                client.say(channel, "Bienvenue sur le panel d'aide du bot");
+				client.say(channel, "Pour l'instant le bot n'a pas de commande.");
+                
+	  break;
+	 
+	   case "createur":
+     client.say(channel,"Je suis le createur de se bot encore en cours de développement");
+   break;
+    case "clear":
+  if(isBroadcaster(user)){
+          violators = [];
+    client.say(channel,"Ont été effacé")
+   }else{
+    client.say(channel,"Bien essayé, "+user+", mais je ne vous obéis pas.");
+  }
+		
+		break;
+		}   
+    //default:
+       //         client.say(channel, "la Commande '" + command + "'' n'existe pas. Tapez " + prefix + "help pour afficher la list du bot " + client.getUsername());
+      //  }
+    } else {
+        let words = message.toLowerCase().split(" ");
+        for(let word of words) {
+            let reaction = reactions[word];
+            if(reaction){
+                client.say(channel, reaction);
+            }
+        }
+    }
+
+});
---
+++ Erina-Twitch/reactions.js
@@ -0,0 +1,11 @@
+const reactions = {
+    "mdr" : "Oh NN quelqun va mourir ;P ",
+	 "jpp" : "Ah ta plus air go te resoucé :)  ",
+     "slt" : "slt poto sa va ?",
+	 "trql" : "ah bas niquel ;)",
+	 "bad" : "regarde le live sa ira mieux :)",
+	 "noob" : "pourtant ta mere ma laisse jouer avec elle",
+   
+   }
+
+module.exports = reactions;
