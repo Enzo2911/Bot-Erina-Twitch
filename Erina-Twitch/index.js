5ea52a0acdfc23fee123611586451651cc82809a
Enzo2911 <38391212+Enzo2911@users.noreply.github.com>
2018-07-03 23:08:19 +0200

    Add files via upload

--- Erina-Twitch/index.js
+++ Erina-Twitch/index.js
@@ -1,3 +1,9 @@
+// Bot Cree par Enzo Merci de ne pas voler le script. !!
+// Vous pouvez bien sur l'améliorer comme vous le souhaité.
+// J espere que se bot vous plaira
+
+
+const say = require('say')
 const tmi = require('tmi.js');
 const prefix = "!";
 const reactions = require('./reactions.js');
@@ -15,20 +21,37 @@ const tmiConfig = {
         password: "oauth:frfk0bd216infrem20gnyp1l9d2z19" // SA NON PLUS
     },
     channels: [
-        "flashzo12345" // A CHANGER SUR LE STREAM QUE VOUS VOULEZ QUE LE BOT SOIS
+        "flashzo12345", "erinatwitchbot", "LE NOM DU CHANEL OU VOUS VOULEZ QUU IL SOIS" // A CHANGER SUR LE STREAM QUE VOUS VOULEZ QUE LE BOT SOIS
     ]
 };
 
 let client = new tmi.client(tmiConfig);
-
+  
 client.connect();
 
 client.on('connected', (adress, port) => {
     console.log(client.getUsername() + " s'est connecté sur : " + adress + ", port : " + port);
-    client.say("votre nom de chaine", "Bonjour Twitch Je suis en train de créé un bot");
-});
+   });
+   
+//-- CECI EST LA PARTIT QUAND UN STREAMEUR REJOINT OU PART -- \\
+
+var onlineMembers = new Array();
+
+client.on("join", function (channel, user, message, self) {
+    onlineMembers.push(user) 
+    console.log(user + " a rejoint le stream")
+
+})
+client.on("part", function (channel, user, message, self) {
+    const index = onlineMembers.indexOf(user);
+    if (index !== -1) {
+        onlineMembers.splice(index, 1);
+    }
+    console.log(user + " a quitter le stream")
+})
 
 
+// ------------------------------------------------------------- \\
 
 function commandParser(message){
     let prefixEscaped = prefix.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
@@ -51,32 +74,59 @@ client.on("chat", function (channel, user, message, self) {
     let sender = user['display-name'];
 
     if(user['mod'] === false){
-        if(message.includes("http:") || message.includes(".com") || message.includes("https:") || message.includes("www.") || message.includes(".fr")){
-            client.timeout(channel, sender, 60, "LIEN INTERDIT !!");
+        if(message.includes("http:") || message.includes(".com") || message.includes("https:") || message.includes("www.") || message.includes(".fr")){ // A NE PAS CHANGER !!!
+            client.timeout(channel, sender, 60, "LIEN INTERDIT !!"); // BAN PENDANT 60 SECONDE
 			client.say(channel, "LIEN INTERDIT : " + user['display-name'] + " / Banni pour 60 seconde")
         }
     }
 
     if(user['mod'] === false){
-        if(message.includes('fdp') || message.includes('salope') || message.includes('ntm') || message.includes('Fdp') || message.includes('Salope') || message.includes('NTM')|| message.includes('Ntm')|| message.includes('FDP')|| message.includes('SALOPE')){
-            client.timeout(channel, sender, 180, "MOT INTERDIT !!");
+        if(message.includes('fdp') || message.includes('salope') || message.includes('ntm') || message.includes('Fdp') || message.includes('Salope') || message.includes('NTM')|| message.includes('Ntm')|| message.includes('FDP')|| message.includes('SALOPE')|| message.includes('pd')|| message.includes('PD') || message.includes('enculé')|| message.includes('ENCULE') || message.includes('salaud')|| message.includes('SALAUD') || message.includes('enfoiré')|| message.includes('ENFOIRE') || message.includes('salaudpard')|| message.includes('tg')|| message.includes('TG')                                                                                                                    ){ // LES DIFFERENT MOTS BANNI PAR LE BOT 
+            client.timeout(channel, sender, 180, "MOT INTERDIT !!"); // BAN PENDANT 180 SECONDE
 			client.say(channel, "MOT INTERDIT : " + user['display-name'] + " / Banni pour 180 seconde")
      
         }
     }
-
+	 if(user['mod'] === false){
+		 
+		  if(message.includes('jul') || message.includes('JUL') ){
+            client.timeout(channel, sender, 300, "MOT STRICTEMENT INTERDIT !!");  // BAN PENDANT 300 SECONDE
+			client.say(channel, "JUL = MOT INTERDIT " + user['display-name'] + " / Banni pour 300 seconde")
+     
+        }
+    }
+  
     if(message == "!youtube") {
-        client.say(channel, "Le lien youtube"); // METTRE SA CHAINE YOUTUBE
+		 client.say(channel, "Ma Chaine Youtube :");
+        client.say(channel, "Le lien de votre chaine youtube"); // METTRE SA CHAINE YOUTUBE
+    }
+	if(message == "!paypal") {
+	    client.say(channel, "N'hesité pas a me faire un don sur cette adresse :"); 
+        client.say(channel, "Le lien Pour que la personne fasse un don a votre paypal"); // METTRE SA LIEN POUR FAIRE UN DON PAYPAL
+    }
+	if(message == "!twitch") {
+		 client.say(channel, "N'hesité pas a aller vous abonné"); 
+        client.say(channel, "Le lien de votre chaine twitch"); // METTRE SA CHAINE TWITCH
+    }
+	if(message == "!snap") {
+		client.say(channel, "N'hesité pas a m'ajouté");
+        client.say(channel, "Le lien de votre snap"); // METTRE SON SNAP
+    }
+	if(message == "!twitter") {
+		client.say(channel, "N'hesité pas a me follow");
+        client.say(channel, "Le lien de votre twitter"); // METTRE SON TWITTER
     }
   if (message == "!quote") {
     	var rand = Quotes[Math.floor(Math.random() * Quotes.length)];
 		client.say(channel, rand);
 	}
-  
+	
+   
  client.action()
    
 });
-client.on('chat', (channel, user, message, isSelf) => {
+  
+client.on('chat', (channel, user, message, isSelf, arg1, arg2) => {
     if (isSelf) return;
 
     let fullCommand = commandParser(message);
@@ -86,7 +136,7 @@ client.on('chat', (channel, user, message, isSelf) => {
         let param = fullCommand[2];
         
         switch(command){
-            case "bonjour":
+            case "bonjour","salut","slt","yo":
     if(isModerator(user)){
         client.say(channel, "Bonjour " + user['display-name'] + ", mon rôle de Bot est de t'aider a géré le live !");
     } else if(isBroadcaster(user)){
@@ -99,21 +149,19 @@ client.on('chat', (channel, user, message, isSelf) => {
                 break;
 		    case "help":
                 client.say(channel, "Bienvenue sur le panel d'aide du bot");
-				client.say(channel, "Pour l'instant le bot n'a pas de commande.");
-                
-	  break;
-	 
+				client.say(channel, "!bonjour, !salut, !slt, !yo // ET le bot vous répondra");
+				client.say(channel, "!createur, Le bot vous parlera de son créateur");
+				client.say(channel, "!youtube cela affichera la chaine youtube du streameur");
+                client.say(channel, "!paypal, Le bot vous affichera un lien paypal pour faire un don");
+				client.say(channel, "!twitch cela affichera la chaine twitch du streameur");
+                client.say(channel, "!snap, Le bot vous donnera le snap du streameur");
+				client.say(channel, "!twitter cela affichera la chaine twitter du streameur");
+                client.say(channel, "!quote cela vous affichera une phrase aléatoire de gameur");
+    
+	 break;
 	   case "createur":
-     client.say(channel,"Enzo est le createur de se bot encore en cours de développement");
-   break;
-    case "clear":
-  if(isBroadcaster(user)){
-          violators = [];
-    client.say(channel,"Ont été effacé")
-   }else{
-    client.say(channel,"Bien essayé, "+user+", mais je ne vous obéis pas.");
-  }
-		
+     client.say(channel,"Enzo est le createur de se bot Anti-Link ( pub ) mais aussi Anti-Insulte, des auto-reaction ...");
+
 		break;
 		}   
     //default:
@@ -128,6 +176,9 @@ client.on('chat', (channel, user, message, isSelf) => {
             }
         }
     }
+	});
 
-});
+		
+       
+
 // BOT CREATE BY ENZO MERCI DE NE PAS S'APPROPRIE SE BOT SA SERAIS UN MANQUE DE RESPECT :)
--- Erina-Twitch/reactions.js
+++ Erina-Twitch/reactions.js
@@ -1,11 +1,21 @@
 const reactions = {
     "mdr" : "Oh NN quelqun va mourir ;P ",
-	 "jpp" : "Ah ta plus air go te resoucé :)  ",
+	 "jpp" : "Ah ta plus air go te resourcé :)  ",
      "slt" : "slt poto sa va ?",
 	 "trql" : "ah bas niquel ;)",
 	 "bad" : "regarde le live sa ira mieux :)",
 	 "noob" : "pourtant ta mere ma laisse jouer avec elle",
-   
-   }
+     "papa" : "J'ai un truc a t'avouer JE SUIS TON PERE", 
+	 "maman" : "j'ai un truc a te dire J'ai coucher avec ton pere :p", 
+	  "chat" : "Pourquoi les chats ont peur de l'eau ? Parce que dans l'eau, minet râle", 
+      "Ovni" : "Il y a deux sortes d'Ovni : L'Ovni tender et l'Ovni true.", 
+      "cochon" : "Qu'est qu'un cochon qui rit ? - Un porc tout gai. ", 
+      "con" : "Jsuis peut etre con, mais j ai decouvert un truc tes plus con que la normal",
+      "ah" : "ah quoi referme ta bouche tout de suite",
+	  "tkt" : "tkt jte banni la prochaine fois :p",
+	  "théorie" : "Mais si 2+2 = 4 pourquoi 2+3 n'est pas egal a 4 OMG TOUT NOTRE MONDE EST TRAFIQUE ONT EST FAIT QU'A OBEIR SANS CONTREDIR",
+      "terre" : "Mais Pourquoi ses la terre qui tourne autour du soleil et pas le soleil qui tourne au tour de la terre car en y repensant le soleil est plus petit que la terre pourquoi sa serais au soleil de rester fix et a la terre d'obeir comme un ...",
+  } 
+   // Vous pouvez rajouté des reaction si tu le souhaite il suffit de revenir a la ligne et de changer les parenthese 
 
 module.exports = reactions;
